5c195a78d18185c934dce85b1a39f391ce69d78b
Jesus Andres Valencia <javaljedy@gmail.com>
2026-04-24 07:50:34 -0500

    Update tailwind.config.js
    Colores base

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'khumeia-cream': '#F7C985',  // El fondo del logo
+        'khumeia-dark': '#4A1C14',   // El marrón de las letras
+        'khumeia-red': '#C8231B',    // Las cerezas del café
+        'khumeia-green': '#6B703F',  // Las hojas de la planta
+        'khumeia-clay': '#9B5D3D',   // El color de la vasija
+      },
+    },
   },
-  plugins: [],
 }
