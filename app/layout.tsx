3ae00b7e2c74afbb3aef675103b0ffd5924d559e
Jesus Andres Valencia <javaljedy@gmail.com>
2026-04-24 12:39:02 -0500

    Update layout.tsx
    Font change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,11 @@
 import './globals.css'
+import { Cinzel } from 'next/font/google' // Importamos una similar
+
+const fontCincoyoStyle = Cinzel({ 
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-cincoyo',
+})
 
 export const metadata = {
   title: 'Khumeia Café | Próximamente',
@@ -7,9 +14,11 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="es">
-      <script src="https://cdn.tailwindcss.com"></script>
-      <body>{children}</body>
+    <html lang="es" className={fontCincoyoStyle.variable}>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </head>
+      <body className="font-serif">{children}</body>
     </html>
   )
 }
